40aca65df51734d7f4d51bad18dd3f8bbc5bbee5
Hannah <hrglasser94@gmail.com>
2018-09-18 09:39:38 -0400

    Added in the react router module and created a few extra pages - an about and a contact page that can be navigated to using NavLinks

--- src/index.js
+++ src/index.js
@@ -3,6 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import HomepageContainer from './HomepageContainer.js'
+import { HashRouter, Route, NavLink, Switch } from 'react-router-dom';
 
-ReactDOM.render(<HomepageContainer />, document.getElementById('root'));
+const About = props => <h1>About</h1>
+const NavBar = props => <div>
+    <NavLink activeStyle={{fontWeight: 'bold'}} exact path to="/">Home</NavLink>
+    <NavLink activeStyle={{fontWeight: 'bold'}} to="/about">About</NavLink>
+    <NavLink activeStyle={{fontWeight: 'bold'}} to="/contact">Contact</NavLink>
+</div>
+
+const Contact = props => <h1>Contact Me!</h1>
+
+const Router = props => {
+     return (
+        <HashRouter>
+            <div>
+                <NavBar></NavBar>
+                <Switch>
+                    <Route exact path='/' component={HomepageContainer}/>
+                    <Route path='/about' component={About}/>
+                    <Route path='/contact' component={Contact}/>
+                </Switch>
+            </div>
+        </HashRouter>
+     )
+ }
+
+ReactDOM.render(<Router />, document.getElementById('root'));
 registerServiceWorker();
